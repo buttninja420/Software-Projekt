f355e3f2699b3bbf94135e8d43590ef1540cda49
buttninja420 <kelvin.rasmussen@gmail.com>
2025-04-01 10:21:20 +0200

    fixed name and added activity list to users

--- src/main/java/dtu/example/ui/User.java
+++ src/main/java/dtu/example/ui/User.java
@@ -1,5 +1,14 @@
 package dtu.example.ui;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class User {
-    
+    String UID;
+    public User(String UID){
+        UID = this.UID;
+        List<Activity> Activities = new ArrayList<Activity>();
+
+    }
+
 }
