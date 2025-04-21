9dc50ca97209a5f538c481e83caecedf114c5ffe
SkedeHede <jeppe@hedeby.dk>
2025-04-21 13:24:53 +0200

    Project changes

--- src/test/java/hellocucumber/StepDefinitionsProject.java
+++ src/test/java/hellocucumber/StepDefinitionsProject.java
@@ -1,5 +1,27 @@
 package hellocucumber;
 
+import dtu.example.ui.Project;
+import io.cucumber.java.en.*;
+import io.cucumber.java.en_scouse.An;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
 public class StepDefinitionsProject {
     
+    private Project project;
+    @Given("a project")
+    public void aProject() {
+        // Initialize app and user logic here
+        project = new Project();
+        // Store UID to use later in the test
+    }
+
+    @Given("a Project with a project leader with UID {string}")
+    public void a_Project_with_a_project_leader_with_UID(String s) {
+        
+    }
+
 }
