0088b83dc2273fcc62f3be6684f79c5c30c9b6f5
NikolajJ2003 <Nikolaj.jorgensen@hotmail.com>
2025-04-21 14:18:32 +0200

    update to StepDefinitionsProject

--- src/test/java/hellocucumber/StepDefinitionsProject.java
+++ src/test/java/hellocucumber/StepDefinitionsProject.java
@@ -10,18 +10,24 @@ import java.util.List;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class StepDefinitionsProject {
-    
-    private Project project;
-    @Given("a project")
-    public void aProject() {
-        // Initialize app and user logic here
-        project = new Project();
-        // Store UID to use later in the test
-    }
-
     @Given("a Project with a project leader with UID {string}")
-    public void a_Project_with_a_project_leader_with_UID(String s) {
-        
+    public void aProjectWithAProjectLeaderWithUID(String string) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
     }
-
+    @When("an employee with UID {string} tries to become project leader")
+    public void anEmployeeWithUIDTriesToBecomeProjectLeader(String string) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @When("Project leader with UID {string} already exists on project")
+    public void projectLeaderWithUIDAlreadyExistsOnProject(String string) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @Then("the employee {string} doesn't become project leader")
+    public void theEmployeeDoesnTBecomeProjectLeader(String string) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
 }
