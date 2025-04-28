dabd92f4b482cc124fb214921438f7098a1e1aef
NikolajJ2003 <Nikolaj.jorgensen@hotmail.com>
2025-04-28 15:27:24 +0200

    uncomment hele project step def filen

--- src/test/java/hellocucumber/StepDefinitionsProject.java
+++ src/test/java/hellocucumber/StepDefinitionsProject.java
@@ -1,47 +1,13 @@
-// package hellocucumber;
+package hellocucumber;
 
-// import dtu.example.ui.*;
-// import io.cucumber.java.en.*;
-// import io.cucumber.java.en_scouse.An;
+import dtu.example.ui.*;
+import io.cucumber.java.en.*;
+import io.cucumber.java.en_scouse.An;
+import java.util.ArrayList;
+import java.util.List;
 
-// import java.util.ArrayList;
-// import java.util.List;
+import static org.junit.jupiter.api.Assertions.*;
 
-// import static org.junit.jupiter.api.Assertions.*;
-
-<<<<<<< HEAD
-// public class StepDefinitionsProject {
-//     Exception thrownException;
-//     private App app;
-//     @Given("An app")
-//     public void anApp() {
-//         // Initialize app and user logic here
-//          app = new App();
-//          // Store UID to use later in the test
-//     }
-//     @Given("a Project {string} with a project leader with UID {string}")
-//     public void aProjectWithAProjectLeaderWithUID(String projectName, String UID) {
-//         app.addProject(projectName);
-//         app.get
-//         throw new io.cucumber.java.PendingException();
-//     }
-//     @When("an employee with UID {string} tries to become project leader")
-//     public void anEmployeeWithUIDTriesToBecomeProjectLeader(String string) {
-//         // Write code here that turns the phrase above into concrete actions
-//         throw new io.cucumber.java.PendingException();
-//     }
-//     @When("Project leader with UID {string} already exists on project")
-//     public void projectLeaderWithUIDAlreadyExistsOnProject(String string) {
-//         // Write code here that turns the phrase above into concrete actions
-//         throw new io.cucumber.java.PendingException();
-//     }
-//     @Then("the employee with UID {string} is not project leader")
-//     public void theemployeewithUIDisnotprojectleader(String string) {
-//         // Write code here that turns the phrase above into concrete actions
-//         throw new io.cucumber.java.PendingException();
-//     }
-// }
-=======
 public class StepDefinitionsProject {
     Exception thrownException;
     private App app;
@@ -74,4 +40,3 @@ public class StepDefinitionsProject {
         throw new io.cucumber.java.PendingException();
     }
 }
->>>>>>> 73fc6eede713a61b922dc15a202e6bfe9f116b25
