f2ffcbe16d23bee54839a4197d181c78389b9e97
SkedeHede <jeppe@hedeby.dk>
2025-04-28 18:05:20 +0200

    Couldn't fix Project :(

--- features/Activity.feature
+++ features/Activity.feature
@@ -1,12 +1,12 @@
 Feature: Activity Register #Jeppe
 
 Scenario: Register to an activity as employee
-  Given an activity with a project leader
+  Given an activity with name "cook" and a project leader
   When an employee with UID "XX12" requests to join an activity
   Then the employee with UID "xx12" is added to the activity
 
 Scenario: Register an employee for an activity as a project leader
-  Given an activity with a project leader and free timeslots
+  Given an activity with name "cook" and a project leader and free timeslots
   When the project leader with UID "XX20" registers an employee with UID "XX21" for the activity
   Then the employee with UID "XX21" is added to the activity
 
--- src/test/java/hellocucumber/StepDefinitionsProject.java
+++ src/test/java/hellocucumber/StepDefinitionsProject.java
@@ -21,10 +21,32 @@ public class StepDefinitionsProject {
          // Store UID to use later in the test
     }
 
+    @Given("a project")
+    public void a_project(String projectName) {
+        tmProject = new Project(projectName);
+    }
+
+    @When("a project with name {string} is created")
+    public void a_project_with_name_is_created(String projectName) {
+        app.addProject(projectName);
+        //System.out.println("project with name: " + projectName + "added!");
+    }
+
+    @Then("the project with name {string} exists")
+    public void the_project_with_name_exists(String projectName) {
+        assertEquals(projectName, tmProject.getName());
+    }
+    
+
     @Given("a Project {string} with a project leader with UID {string}")
     public void aProjectWithAProjectLeaderWithUID(String projectName, String UID) {
+        // app.addProject(projectName);
+        // tmProject =  app.getProject(projectName);
+        // app.registerUser(UID);
+        // tmProject.setProjectLeader(app.getUserWithUID(UID));
+        // throw new io.cucumber.java.PendingException();
+        tmProject = new Project(projectName);
         app.addProject(projectName);
-        tmProject =  app.getProject(projectName);
         app.registerUser(UID);
         tmProject.setProjectLeader(app.getUserWithUID(UID));
         throw new io.cucumber.java.PendingException();
@@ -38,8 +60,31 @@ public class StepDefinitionsProject {
 
     @Then("the employee with UID {string} is not project leader")
     public void theemployeewithUIDisnotprojectleader(String name){
-        assertEquals(name,tmProject.getProjectleader());
-
-        throw new io.cucumber.java.PendingException();
+        assertNotEquals(name,tmProject.getProjectleader());
     }
+
+    @Given("a Project with name {string}")
+    public void a_Project_with_name(String projectName) {
+        projectName = tmProject.getName();
+    }
+
+    @When("an employee who is not the project leader adds new activity with name {string}")
+    public void an_employee_who_is_not_the_project_leader_adds_new_activity_with_name(String activityName) {
+        Activity activity1 = new Activity();
+        tmProject.addActivity(activity1);
+        activity1.setTitle(activityName);
+    }
+
+    @Then("the Project has an activity named {string}")
+    public void the_Project_has_an_activity_named(String activityName) {
+        boolean found = false;
+        for (Activity activity : tmProject.getActivities()) {
+            if (activity.getTitle().equals(activityName)) {
+                found = true;
+                break;
+            }
+        }
+        assertTrue(found);
+    }
+
 }
